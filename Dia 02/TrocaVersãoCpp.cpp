a5d56e78a4b0a418ff8d1c390bdae11dde96f115
Keymiuz <jpcicolo@gmail.com>
2025-04-14 05:51:25 -0300

    Evitando erros com C++

---
+++ Dia 02/TrocaVersãoCpp.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <iostream>
+
+void trocarValores(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+    printf("Antes da troca: x = %d, y = %d\n", x, y); 
+    trocarValores(x, y);
+    printf("Depois da troca: x = %d, y = %d\n", x, y); 
+    return 0;
+}
