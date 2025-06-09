971316b00497007eb6b2f2b3df11f4b0a6bc2fc0
Keymiuz <jpcicolo@gmail.com>
2025-06-08 22:07:09 -0300

    Mais operações, visualização com o modo debug e correção/aprimoramento da operação em polinômios

--- Dia 11/PolyV2.c
+++ Dia 11/PolyV2.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 struct Node {
     int exp, coeff;
     struct Node *next;
 }*poly=NULL;
+
+// Função auxiliar para calcular x^n de forma eficiente para inteiros
+long long power(int base, int exp) {
+    long long res = 1;
+    for (int i = 0; i < exp; i++) {
+        res *= base;
+    }
+    return res;
+}
 
 void createPolinomial() {
     struct Node *t, *last;
@@ -25,7 +33,7 @@ void createPolinomial() {
             last = t;
         }
     }
-}   
+}    
 
 void Display(struct Node *p) {
     while(p!=NULL) {
@@ -38,7 +46,8 @@ void Display(struct Node *p) {
 long Eval(struct Node *p, int x) {
     long val = 0;
     while(p!=NULL) {
-        val += p->coeff * pow(x, p->exp);
+        // Usamos nossa função 'power' otimizada em vez de 'pow'
+        val += p->coeff * power(x, p->exp); 
         p = p->next;
     }
     return val;
@@ -53,17 +62,3 @@ int main() {
     printf("Valor do polinomio em x = %d: %ld\n", x, Eval(poly, x));
     return 0;
 }
-
-
-// representação de um polinômio com a lista encadeada 
-// é uma lista encadeada onde cada nó contém o coeficiente e o expoente de um termo do polinômio
-
-
-
-// Listas encadeadas são excelentes para representar estruturas de dados onde o número de elementos não é fixo ou onde há muitos "buracos" (termos com coeficiente zero) no polinômio.
-// Por exemplo, se temos um polinômio de grau 1000, mas apenas os termos de grau 500 e 1000 são não nulos, uma lista encadeada é uma representação eficiente.
-
-
-
-// Embora neste exemplo a inserção seja sempre no final (o que é simples com last apontando para o último nó), em cenários mais complexos (como adicionar dois polinômios, onde novos termos podem precisar ser inseridos em ordem)
-//  listas encadeadas podem facilitar a inserção em qualquer posição sem a necessidade de "shiftar" elementos, como seria necessário em um array.
